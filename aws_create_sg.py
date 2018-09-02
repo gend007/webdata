23a2ea4efa8bfd5edfa4ba73becc453e0187cfc1
gend007 <42605557+gend007@users.noreply.github.com>
2018-09-02 09:58:24 +0900

    update_20180902

--- aws_create_sg.py
+++ aws_create_sg.py
@@ -1,18 +1,26 @@
 import subprocess
 
+### セキュリティグループ作成用変数
 sg_name = "test"
 vpc_id = "vpc-d1dc8bb6"
 des = "test_sg"
-
 port_id = "80"
 ip_add = "223.218.160.243/32"
 
+err_c = 1
+nomal_c = 0
 
+### セキュリティグループ作成
 cmd_sg = "aws ec2 create-security-group --group-name " + sg_name + " --vpc-id " + vpc_id + " --description " + des + " --output text"
-group_id = subprocess.check_output(cmd_sg, shell=True)
-print (group_id.decode('sjis'))
+try:
+    group_id = subprocess.check_output(cmd_sg, shell=True)
+    #print (group_id.decode('sjis').strip())
+except subprocess.CalledProcessError as e:
+    sys.exit(err_c)
 
-
-cmd_sg_in = "aws ec2 authorize-security-group-ingress --group-id " + group_id.decode('sjis') + " --protocol tcp --port " + port_id + " --cidr " + ip_add
-print(cmd_sg_in)
+### 作成したセキュリティグループのインバウント設定
+cmd_sg_in = "aws ec2 authorize-security-group-ingress --group-id " + group_id.decode('sjis').strip() + " --protocol tcp --port " + port_id + " --cidr " + ip_add
+#print(cmd_sg_in.strip())
 subprocess.run(cmd_sg_in, shell=True)
+
+sys.exit(nomal_c)
