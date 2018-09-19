caa66eda841c64456d7c765c7e9aba1762a7308a
gend007 <42605557+gend007@users.noreply.github.com>
2018-09-20 08:34:54 +0900

    update

--- test.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-cmd = "aws ec2 describe-instances --query Reservations[*].Instances[*].InstanceId"
-
-output = subprocess.check_output(cmd, shell=True)
-
-print (output.decode('sjis'))
